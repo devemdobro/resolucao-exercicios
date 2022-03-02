eaf95ee095d10fc5a831001daa78cb82d3eb0630
Roberto Dias <roberto.rhd@gmail.com>
2022-03-02 10:47:01 -0300

    fixes

--- exercicios-html-css-js-intermediario/exercicio-01/src/js/index.js
+++ exercicios-html-css-js-intermediario/exercicio-01/src/js/index.js
@@ -1,31 +1,54 @@
-let posicaoImagemAtual = 0;
-const setaAvancar = document.getElementById("seta-avancar");
-const setaVoltar = document.getElementById("seta-voltar");
 const imagens = document.querySelectorAll(".imagem");
+const setaVoltar = document.getElementById("seta-voltar");
+const setaAvancar = document.getElementById("seta-avancar");
+let imagemAtual = 0;
 
-setaVoltar.addEventListener("click", avancarImagem);
+setaAvancar.addEventListener("click", function () {
+  if (imagemAtual !== imagens.length - 1) {
+    imagemAtual++;
+  }   
+  
+  esconderImagens();
+  mostrarImagem();
+  mostrarOuEsconderSetas();
+});
 
-setaAvancar.addEventListener("click", voltarImagem);
-
-function avancarImagem() {    
-  const totalImagens = imagens.length;
-  console.log(imagens)
-  if (posicaoImagemAtual === totalImagens) {
-    return;
+setaVoltar.addEventListener("click", function () {
+  if (imagemAtual !== 0) {
+    imagemAtual--;
   }
 
-  posicaoImagemAtual++;  
+  esconderImagens();
+  mostrarImagem();
+  mostrarOuEsconderSetas();
+});
 
-  imagens.forEach(function(imagemAtual){
-      console.log(imagemAtual)
+function esconderImagens() {
+  imagens.forEach((imagemAtual) => {
+    imagemAtual.classList.remove("mostrar");
   });
-
 }
 
-function voltarImagem() {
-  if (posicaoImagemAtual === 0) {
-    return;
-  }
+function mostrarImagem() {
+  imagens[imagemAtual].classList.add("mostrar");
+}
 
-  posicaoImagemAtual--;
+function mostrarOuEsconderSetas(){
+    //ver se a imagem atual é dirente de 0, se sim pode habilitar a seta de voltar
+    if(imagemAtual !== 0){
+        setaVoltar.classList.remove("opacidade");
+    }
+    //se a imagem atual for igual a 0, se sim desabilita a seta de voltar
+    else if(imagemAtual === 0){
+        setaVoltar.classList.add("opacidade");
+    }
+
+    //ver se a imagem atual é igual ao tamanho do array total de imagens, se sim pode esconder a seta de avancar
+    if(imagemAtual !== 0 && imagemAtual === imagens.length - 1){
+        setaAvancar.classList.add("opacidade");
+    }
+    //se a imagem atual for diferente do tamanho do array total de imagens, habilito a seta de avancar
+    else if(imagemAtual !== imagemAtual.length -1){
+        setaAvancar.classList.remove("opacidade");
+    }    
 }
