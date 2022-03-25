ea8a9b93cddc9c4321ba5bf51ffa26749d3dd56b
Roberto Dias <roberto.rhd@gmail.com>
2022-03-25 12:07:59 -0300

    adiciona links no index.html

--- exercicios-html-css-js-intermediario/exercicio-2/src/js/acordeon.js
+++ exercicios-html-css-js-intermediario/exercicio-2/src/js/acordeon.js
@@ -3,6 +3,7 @@ const itensPerguntasERespostas = document.querySelectorAll(".item");
 itensPerguntasERespostas.forEach(function (item) {
   item.addEventListener("click", function () {
     const itemAtivoAtual = document.querySelector(".ativo");
+    
     if (itemAtivoAtual) {
       itemAtivoAtual.classList.remove("ativo");
     }
